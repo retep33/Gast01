97a6513b46c2ed93cbe549661fc541b38fdc9d4f
Palotás Péter <Palots Pter>
2016-04-25 10:53:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/felhasznalo/gast01/Felpakol.java
@@ -0,0 +1,89 @@
+package com.example.felhasznalo.gast01;
+
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class Felpakol extends AppCompatActivity {
+
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_felpakol);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void scanBar (View v){
+        try {
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException e) {
+            showDialog(this, "Nem találok szkennert", "Letöltsek egyet?", "Igen", "NEM").show();
+        }
+
+
+
+    }
+
+    private static AlertDialog showDialog(final Felpakol act, CharSequence title, CharSequence message,
+                                          CharSequence buttonYes, CharSequence buttonNo){
+
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title).setMessage(message).setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return downloadDialog.show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(requestCode == 0){
+            if (resultCode == RESULT_OK){
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast.makeText(this, "TARTALOM: " + contents + "Formátum: " + format, Toast.LENGTH_LONG ).show();
+            }
+        }
+    }
+}
